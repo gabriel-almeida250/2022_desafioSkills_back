17a70d127d69b55ee85f15492f7eede81790a135
Gabriel Almeida <gabrielalmeida2612@outlook.com>
2022-08-16 16:09:17 -0300

    Iniciando o projeto de back end

---
+++ projetoNeki/src/test/java/com/neki/projetoNeki/ProjetoNekiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neki.projetoNeki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
